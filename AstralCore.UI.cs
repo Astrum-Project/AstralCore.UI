5ba4a53c9840f72add868e4e1f572762412e506b
xKiraiChan <xKiraiChan@gmail.com>
2021-12-18 12:36:31 -0500

    allow for scanning individual assemblies

--- AstralCore.UI.cs
+++ AstralCore.UI.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Reflection;
 using Astrum.AstralCore.UI.Attributes;
 
-[assembly: MelonInfo(typeof(Astrum.AstralCore.UI.CoreUI), "AstralCore.UI", "1.1.1", downloadLink: "github.com/Astrum-Project/AstralCore.UI")]
+[assembly: MelonInfo(typeof(Astrum.AstralCore.UI.CoreUI), "AstralCore.UI", "1.2.0", downloadLink: "github.com/Astrum-Project/AstralCore.UI")]
 [assembly: MelonColor(ConsoleColor.DarkMagenta)]
 
 namespace Astrum.AstralCore.UI
@@ -27,21 +27,29 @@ namespace Astrum.AstralCore.UI
 
             Modules.Clear();
 
-            OnPreScan();
+            OnPreScan(); // this is for if you want to scan your own assembly that isn't a mod (WorldMods)
 
-            foreach ((MemberInfo info, UIBase attr) in MelonHandler.Mods
-                .Select(x => x.Assembly)
-                .SelectMany(x => x.GetExportedTypes())
+            MelonHandler.Mods.ForEach(x => ScanAssembly(x.Assembly));
+
+            sw.Stop();
+            Logger.Debug("[Core.UI] Scanned in " + sw.ElapsedMilliseconds + " ms");
+
+            OnRescan(); // this is for recalculating your UI (UI.Wings)
+        }
+
+        public static void ScanAssembly(Assembly assembly)
+        {
+            foreach ((MemberInfo info, UIBase attr) in assembly.GetExportedTypes()
                 .SelectMany(x => x.GetMembers())
                 .SelectMany(x => x.GetCustomAttributes(typeof(UIBase), false).Select(y => (x, y as UIBase))))
             {
                 if (!Modules.TryGetValue(attr.Module, out var module))
                     module = Modules[attr.Module] = new Module() { Commands = new Dictionary<string, UIBase>(StringComparer.OrdinalIgnoreCase) };
 
-                try 
+                try
                 {
                     attr.GetType().GetMethod("Setup", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(attr, new object[1] { info });
-                } 
+                }
                 catch
                 {
                     Logger.Error($"An exception has occurred whilst setting up {attr.Module}:{attr.Name}");
@@ -49,11 +57,6 @@ namespace Astrum.AstralCore.UI
 
                 module.Commands[attr.Name] = attr;
             }
-
-            OnRescan();
-
-            sw.Stop();
-            Logger.Debug("[Core.UI] Scanned in " + sw.ElapsedMilliseconds + " ms");
         }
     }
 }
